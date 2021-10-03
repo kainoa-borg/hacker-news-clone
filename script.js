3cd87543c0b6536056d775a798638d629c073f42
kaimasaru <kainoa.accs@gmail.com>
2021-10-03 15:17:11 -0600

    Initial commit for new feature branch to add single-story view to HN clone. Not much has been implemented yet, but this initial commit also fixes #10, and closes #7

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-/* eslint linebreak-style: ["error", "windows"]*/
+/* eslint linebreak-style: ["error", "windows"] */
+
+/** Takes int seconds and returns converted minutes or hours */
+const calculateTime = (seconds) => {
+  const minutesMsg = ' minutes ago'
+  const secsSinceNow = Math.floor(Date.now() / 1000) - seconds;
+  let returnVal = undefined;
+  
+  if (secsSinceNow < 60) returnVal =  1 + minutesMsg;
+  else if (secsSinceNow < 3600) returnVal =  Math.floor(secsSinceNow / 60) + minutesMsg;
+  
+  return returnVal;
+}
 
 /** Takes a Story object 'story' and returns a DOM span element 'sub' that contains data on subline of story  */
 const storySubBuilder = (story) => {
   const sub = document.createElement('span');
+  
   pScore = document.createElement('p');
   pScore.innerHTML = story.score + ' points by ';
+  
   pUser = document.createElement('p');
   pUser.innerHTML = story.by;
+  
+  pTime = document.createElement('a');
+  pTime.setAttribute('href', '#' + story.id);
+  if (story.time) {
+    pTime.innerHTML = calculateTime(story.time);
+  }
   sub.appendChild(pScore);
   sub.appendChild(pUser);
+  sub.appendChild(pTime);
+  
   return sub;
 };
 
@@ -16,22 +38,28 @@ const storySubBuilder = (story) => {
 const storyMainBuilder = (story, index) => {
   const main = document.createElement('span');
   main.setAttribute('class', 'post-story');
+  
   pIndex = document.createElement('p');
   pIndex.setAttribute('class', 'index');
   pIndex.innerHTML = index;
+  
   pUpvote = document.createElement('button');
   pUpvote.innerHTML = '^';
+  
   pTitle = document.createElement('a');
   pTitle.href = story.url;
   pTitle.innerHTML = story.title;
+  
   pDomain = document.createElement('p');
   pDomain.setAttribute('class', 'domain');
   pDomain.innerHTML = '(' + pTitle.hostname + ')';
+  
   main.appendChild(pIndex);
   main.appendChild(pUpvote);
   main.appendChild(pTitle);
   main.appendChild(pDomain);
   main.setAttribute('class', 'post-story');
+  
   return main;
 };
 
@@ -47,13 +75,14 @@ const storyElementBuilder = (story, index) => {
 
 /** Takes an array of Story objects 'storyArray' and returns a page container populated with 'story' elements */
 const buildPage = (storyArray) => {
-  console.log(storyArray);
   container = document.getElementById('posts');
   container.innerHTML = '';
+  
   for (let i = 0; i < storyArray.length; ++i) {
     // Create a story div for each story
     container.appendChild(storyElementBuilder(storyArray[i], i + 1));
   }
+  
   return container;
 };
 
@@ -63,9 +92,10 @@ const buildPage = (storyArray) => {
 const fetchStoryJSON = async (itemID) => {
   url = 'https://hacker-news.firebaseio.com/v0/item/' + itemID + '.json?';
   const response = await fetch(url);
-  if (response.ok) {
-    return response.json();
-  }
+  
+  if (response.ok) return response.json();
+
+  else return;
 };
 
 /** Takes a JSON object of a page view from HN Firebase API and returns an array of Story objects from each story's JSON */
@@ -88,6 +118,13 @@ const loadPage = (pageLink) => {
       .then((storyArray) => buildPage(storyArray));
 };
 
+const loadPost = (postID) => {
+  fetchStoryJSON(postID)
+      .then((JSON) => {
+        console.log(JSON);
+      });
+}
+
 /** Callback function for onhashchange event
  * Loads page given by location.hash
  */
@@ -103,11 +140,18 @@ const locationHashChanged = () => {
     }
     case '#home': {
       loadPage('/topstories');
+      break;
+    }
+    default: {
+      console.log(location.hash.slice(1, location.hash.length - 1));
+      loadPost(location.hash.slice(1, location.hash.length - 1));
     }
   }
 };
+container = document.getElementById('posts');
+
+locationHashChanged()
 
 console.log('Hello, World!');
 
-loadPage('/topstories');
 window.onhashchange = locationHashChanged;
