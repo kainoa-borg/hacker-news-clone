2041ca13a5f14a558304c4e6c11a95e433e64fcc
kaimasaru <kainoa.accs@gmail.com>
2021-10-02 22:10:40 -0600

    Another major refactor, this time focusing on cleaning up the code as well as fixing async wonkiness. Changes in where and how async functions are used has increased performance dramatically and no longer results in undefined behavior. This branch closes #1 fixes #2 and closes #3

--- script.js
+++ script.js
@@ -1,61 +1,88 @@
 /* eslint linebreak-style: ["error", "windows"]*/
 
-/** Represents a parent class for all DOM element types */
-
-const handleJSON = (JSON) => {
-  return JSON;
+const postSubBuilder = (post) => {
+  const sub = document.createElement('span');
+  pScore = document.createElement('p');
+  pScore.innerHTML = post.score + ' points by ';
+  pUser = document.createElement('p');
+  pUser.innerHTML = post.by;
+  sub.appendChild(pScore);
+  sub.appendChild(pUser);
+  return sub;
 };
 
-const fetchPageJSON = async (pageLink) => {
-  // Create full url to request from hacker-news api
-  url = 'https://hacker-news.firebaseio.com/v0' + pageLink + '.json?';
-  // console.log(url);
-  const response = await fetch(url);
-  if (response.ok) {
-    return await response.json();
-  }
+const postMainBuilder = (post, index) => {
+  const main = document.createElement('span');
+  main.setAttribute('class', 'post-story');
+  pIndex = document.createElement('p');
+  pIndex.setAttribute('class', 'index');
+  pIndex.innerHTML = index;
+  pUpvote = document.createElement('button');
+  pUpvote.innerHTML = '^';
+  pTitle = document.createElement('a');
+  pTitle.href = post.url;
+  pTitle.innerHTML = post.title;
+  pDomain = document.createElement('p');
+  pDomain.setAttribute('class', 'domain');
+  pDomain.innerHTML = '(' + pTitle.hostname + ')';
+  main.appendChild(pIndex);
+  main.appendChild(pUpvote);
+  main.appendChild(pTitle);
+  main.appendChild(pDomain);
+  main.setAttribute('class', 'post-story');
+  return main;
+};
+
+const postElementBuilder = (post, index) => {
+  const story = document.createElement('div');
+
+  story.append(postMainBuilder(post, index));
+  story.append(postSubBuilder(post));
+
+  return story;
 };
 
 const buildPage = (storyArray) => {
+  console.log(storyArray);
   container = document.getElementById('posts');
+  container.innerHTML = '';
   for (let i = 0; i < storyArray.length; ++i) {
     // Create a story div for each story
-    storyDiv = document.createElement('div');
-    container.appendChild(storyDiv);
-    storyTitle = document.createElement('p');
-    storyDiv.appendChild(storyTitle);
-    storyTitle.innerHTML = storyArray[i].title;
+    container.appendChild(postElementBuilder(storyArray[i], i + 1));
   }
   return container;
 };
 
-const buildStoryArray = async (JSON) => {
-  const storyArray = [];
-  for (let i = 0; i < 30; ++i) {
-    const story = await fetchPageJSON('/item/' + JSON[i]);
-    if (story) {
-      // console.log(story);
-      storyArray.push(story);
-    }
+const fetchStoryJSON = async (pageLink) => {
+  url = 'https://hacker-news.firebaseio.com/v0/item/' + pageLink + '.json?';
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.json();
   }
-
-  return storyArray;
 };
 
-const loadPage = (pageID) => {
-  fetchPageJSON(pageID)
-      .then((value) => {
-        return buildStoryArray(value);
-      })
-      .then((value) => {
-        return buildPage(value);
-      });
+const buildStoryArray = (JSON) => {
+  const promiseArray = [];
+  for (i = 0; i < 30; ++i) {
+    promiseArray.push(fetchStoryJSON(JSON[i]));
+  }
+  return Promise.all(promiseArray);
+};
+
+const loadPage = (pageLink) => {
+  // Create full url to request from hacker-news api
+  url = 'https://hacker-news.firebaseio.com/v0' + pageLink + '.json?';
+  // console.log(url);
+  fetch(url)
+      .then((response) => response.json())
+      .then((JSON) => buildStoryArray(JSON))
+      .then((storyArray) => buildPage(storyArray));
 };
 
 const locationHashChanged = () => {
   switch (location.hash) {
-    case '#news': {
-      loadPage('/topstories');
+    case '#best': {
+      loadPage('/beststories');
       break;
     }
     case '#newest': {
@@ -66,9 +93,8 @@ const locationHashChanged = () => {
       loadPage('/item/3000');
       break;
     }
-    default: {
-      fetchPageJSON('/topstories')
-          .then((value) => handleJSON(value));
+    case '#home': {
+      loadPage('/topstories');
     }
   }
 };
